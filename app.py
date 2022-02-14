bfef2389af283f6bb927e8163a8395f79ad48f7d
artdevi <tar.19@uni-dubna.ru>
2022-02-14 22:08:31 +0300

    added contact info GET request

--- app.py
+++ app.py
@@ -1,62 +1,11 @@
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
-conn = psycopg2.connect(dbname='database', user='db_user', password='mypassword', host='localhost')
-cursor = conn.cursor()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/testdb'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-
-#
-# class Contract(db.Model):
-#     __tablename__ = 'contracts'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     name = db.Column(db.String(150), nullable=False)
-#     balance = db.Column(db.Integer, nullable=False, default=0)
-#     is_legal_entity = db.Column(db.Boolean, nullable=False)
-#     is_active = db.Column(db.Boolean, nullable=False, default=True)
-#
-#     def __init__(self, name, is_legal_entity, is_active=True):
-#         self.name = name
-#         self.is_legal_entity = is_legal_entity
-#         self.is_active = is_active
-#
-#
-# class Address(db.Model):
-#     __tablename__ = 'addresses'
-#     id = db.Column(db.Integer, primary_key=True)
-#     contract_id = db.Column(db.Integer, db.ForeignKey('contracts.id'), nullable=False)
-#     city = db.Column(db.String(100), nullable=False)
-#     street = db.Column(db.String(100), nullable=False)
-#     house = db.Column(db.String(16), nullable=False)
-#     apartment = db.Column(db.Integer, nullable=False)
-#
-#
-# class Tariff(db.Model):
-#     __tablename__ = 'tariffs'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(64), nullable=False)
-#     price = db.Column(db.Integer, nullable=False)
-#     start_date = db.Column(db.Date, nullable=False)
-#     end_date = db.Column(db.Date)
-#
-#
-# class Transaction(db.Model):
-#     __tablename__ = 'transactions'
-#     id = db.Column(db.Integer, primary_key=True)
-#     contract_id = db.Column(db.Integer, db.ForeignKey('contracts.id'), nullable=False)
-#     sum = db.Column(db.Integer, nullable=False)
-#     datetime = db.Column(db.DateTime, nullable=False)
-#
-#
-# db.create_all()
-# # contract = Contract('Test Name', True, True)
-# # db.session.add(contract)
-# # db.session.commit()
+conn = psycopg2.connect(dbname='testdb', user='postgres', password='password', host='localhost')
+cursor = conn.cursor()
 
 
 @app.route('/')
@@ -66,8 +15,14 @@ def home():
 
 @app.route('/contract/<id>', methods=['GET'])
 def get_contract_info(id):
-    # contact = Contract.query.get(id)
-    return jsonify()
+    cursor.execute(f'SELECT * FROM contracts LEFT JOIN addresses ON contracts.id = addresses.contract_id WHERE contracts.id = {id}')
+
+    data = cursor.fetchone()
+
+    if data is not None:
+        return jsonify(data)
+    else:
+        return 'No such contracts, please check the correctness of the entered data', 400
 
 
 @app.route('/balance/<id>', methods=['GET'])
